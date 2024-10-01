5336ec597acbc7e832da373f82d3de067d31cb24
Pas-nou <pascal.renou@live.fr>
2024-10-01 19:15:35 +0200

    home in progress for mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,50 +23,47 @@ function Home() {
     const settings: Settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 1500,
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptativeHeight: true,
-        // autoplay: true,
-        // autoplaySpeed: 4500,
-        // pauseOnHover: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: true,
     }
     return (
         <section className="home_container">
             <Slider {...settings} className="home_slider">
-                <article className="slide_container_1">
+                <article className="slide_container slide_container_1">
                     <div className="slide_text_container">
-                        <h3 className="slide_title_1">SAISON 2024 OUVERTE !</h3>
+                        <h3 className="slide_title slide_title_1">SAISON 2024 OUVERTE !</h3>
                         <div className="slide_text_descriptif">
-                            <p className="slide_text_1">l'ASTHEP vous présente une pièce</p>
-                            <p className="slide_text_1">écrite et mise en scène par</p>
-                            <p className="slide_text_1">Olivier Jollivet.</p>
-                            <p className="slide_text_1">Représentations les :</p>
-                            <p className="slide_text_1">09 | 10 | 15 | 16 | 17 | 19</p>
-                            <p className="slide_text_1">22 | 23 | 24 | 29 | 30 </p>
-                            <p className="slide_text_1">Novembre.</p>
+                            <p className="slide_text slide_text_1">l'ASTHEP vous présente une pièce</p>
+                            <p className="slide_text slide_text_1">écrite et mise en scène par</p>
+                            <p className="slide_text slide_text_1">Olivier Jollivet.</p>
+                            <p className="slide_text slide_text_1">Représentations les :</p>
+                            <p className="slide_text slide_text_1">09 | 10 | 15 | 16 | 17 | 19</p>
+                            <p className="slide_text slide_text_1">22 | 23 | 24 | 29 | 30 </p>
+                            <p className="slide_text slide_text_1">Novembre.</p>
                         </div>
                         <NavLink className="slide_button_1" to="/billetterie">Réservez vos places</NavLink>
                     </div>
                     <img className="slide_image_1" src="../../public/Affiche_fleur_ruines.jpg" alt="Affiche de la pièce de théâtre 'Une fleur sur les ruines'" />
                 </article>
-                <article className="slide_container_2">
-                    <div>
-                        <h3>Coucou 2</h3>
-                        <p>Ici le texte de la slide</p>
+                <article className="slide_container slide_container_2">
+                    <div className="slide_text_container">
+                        <h3 className="slide_title slide_title_2">L'ASTHEP SUR LE WEB !</h3>
+                        <div className="slide_text_descriptif">
+                            <p className="slide_text slide_text_2">L'ASTHEP lance son site web !</p>
+                            <p className="slide_text slide_text_2">Après le rafraîchissement de sa salle et de son logo,</p>
+                            <p className="slide_text slide_text_2">la troupe se met au goût du jour</p>
+                            <p className="slide_text slide_text_2">et vous propose un système de billetterie en ligne.</p>
+                            <p className="slide_text slide_text_2">Vous pourrez également retrouver notre actualité,</p>
+                            <p className="slide_text slide_text_2">l'historique des pièces jouées,</p>
+                            <p className="slide_text slide_text_2">et plein d'autres choses à venir ! </p>
+                        </div>
                     </div>
-                </article>
-                <article className="slide_container_3">
-                    <div>
-                        <h3>Coucou 3</h3>
-                        <p>Ici le texte de la slide</p>
-                    </div>
-                </article>
-                <article className="slide_container_3">
-                    <div>
-                        <h3>Coucou 4</h3>
-                        <p>Ici le texte de la slide</p>
-                    </div>
+                    <img className="slide_image_2" src="../../public/Logo_asthep.png" alt="Logo de la troupe de l'ASTHEP" />
                 </article>
             </Slider>
         </section >
