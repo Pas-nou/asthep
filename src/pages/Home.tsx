77b2d80ae86af70c1b8a4d13b500a2300a373d1d
Pas-nou <pascal.renou@live.fr>
2024-10-01 18:33:57 +0200

    home in progress, desktop ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { NavLink } from "react-router-dom";
 import Slider from "react-slick";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import '../css/Home.css'
+
 
 function Home() {
 
@@ -25,31 +27,49 @@ function Home() {
         slidesToShow: 1,
         slidesToScroll: 1,
         adaptativeHeight: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: true,
+        // autoplay: true,
+        // autoplaySpeed: 4500,
+        // pauseOnHover: true,
     }
     return (
         <section className="home_container">
             <Slider {...settings} className="home_slider">
-                <div className="test_1">
-                    <h2>Coucou 1</h2>
-                    <p>Ici le texte de la slide</p>
-                </div>
-                <div className="test_2">
-                    <h2>Coucou 2</h2>
-                    <p>Ici le texte de la slide</p>
-                </div>
-                <div className="test_3">
-                    <h2>Coucou 3</h2>
-                    <p>Ici le texte de la slide</p>
-                </div>
-                <div className="test_4">
-                    <h2>Coucou 4</h2>
-                    <p>Ici le texte de la slide</p>
-                </div>
+                <article className="slide_container_1">
+                    <div className="slide_text_container">
+                        <h3 className="slide_title_1">SAISON 2024 OUVERTE !</h3>
+                        <div className="slide_text_descriptif">
+                            <p className="slide_text_1">l'ASTHEP vous présente une pièce</p>
+                            <p className="slide_text_1">écrite et mise en scène par</p>
+                            <p className="slide_text_1">Olivier Jollivet.</p>
+                            <p className="slide_text_1">Représentations les :</p>
+                            <p className="slide_text_1">09 | 10 | 15 | 16 | 17 | 19</p>
+                            <p className="slide_text_1">22 | 23 | 24 | 29 | 30 </p>
+                            <p className="slide_text_1">Novembre.</p>
+                        </div>
+                        <NavLink className="slide_button_1" to="/billetterie">Réservez vos places</NavLink>
+                    </div>
+                    <img className="slide_image_1" src="../../public/Affiche_fleur_ruines.jpg" alt="Affiche de la pièce de théâtre 'Une fleur sur les ruines'" />
+                </article>
+                <article className="slide_container_2">
+                    <div>
+                        <h3>Coucou 2</h3>
+                        <p>Ici le texte de la slide</p>
+                    </div>
+                </article>
+                <article className="slide_container_3">
+                    <div>
+                        <h3>Coucou 3</h3>
+                        <p>Ici le texte de la slide</p>
+                    </div>
+                </article>
+                <article className="slide_container_3">
+                    <div>
+                        <h3>Coucou 4</h3>
+                        <p>Ici le texte de la slide</p>
+                    </div>
+                </article>
             </Slider>
-        </section>
+        </section >
     );
 };
 
