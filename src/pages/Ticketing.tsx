05e7cef2aee86e8167f4f1256df3ca14bbac31d2
Pas-nou <pascal.renou@live.fr>
2024-10-04 13:45:47 +0200

    optimisation of tickting page

--- src/pages/Ticketing.tsx
+++ src/pages/Ticketing.tsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 import "../css/Ticketing.css"
 
 function Ticketing() {
 
     // Test d'un useEffect sur l'intégration via BilletWeb
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://www.billetweb.fr/js/export.js";
-        script.async = true;
-        document.body.appendChild(script);
+    // useEffect(() => {
+    //     const script = document.createElement("script");
+    //     script.src = "https://www.billetweb.fr/js/export.js";
+    //     script.async = true;
+    //     document.body.appendChild(script);
 
-        // Nettoyage lors du démontage
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+    //     // Nettoyage lors du démontage
+    //     return () => {
+    //         document.body.removeChild(script);
+    //     };
+    // }, []);
 
     return (
         <div className="ticketing_container">
             <h2 className='ticketing_title'>Votre réservation</h2>
             <section className='ticketing_reservation'>
-                <a
+                {/* <a
                     title='Vente de billet en ligne'
                     href="https://www.billetweb.fr/shop.php?event=theatre-poiteviniere"
                     className='shop_frame'
@@ -32,7 +32,21 @@ function Ticketing() {
                     data-initial-height="600"
                     data-scrolling="no"
                     data-id="theatre-poiteviniere"
-                    data-resize="1" />
+                    data-resize="1" /> */}
+                    <iframe
+                    title="Vente de billet en ligne"
+                    className='shop_frame'
+                    rel='noopener noreferrer'
+                    src="https://www.billetweb.fr/shop.php?event=theatre-poiteviniere"
+                    data-src="https://www.billetweb.fr/shop.php?event=theatre-poiteviniere"
+                    width="100%"
+                    data-max-width="90%"
+                    height="880"
+                    data-scrolling="no"
+                    data-id="theatre-poiteviniere"
+                    data-resize="1"
+                    frameBorder="0"
+                 />
             </section>
         </div>
     )
